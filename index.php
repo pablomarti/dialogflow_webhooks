26706cc33b0035fb432bb699435c1e9ab93eff16
Pablo Marti <startpablo@gmail.com>
2019-01-03 16:18:12 -0600

    Add log of data of Dialogflow

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!empty($_POST)) {
+  $file = 'log.txt';
+  $current = file_get_contents($file);
+
+  foreach($_POST as $key => $value) {
+    $current .= "$key : $value\n";
+  }
+
+  // Set log of data sent by Dialogflow
+  file_put_contents($file, $current);
+}
+
+?>
